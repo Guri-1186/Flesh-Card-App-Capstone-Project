4fc994c62918ba94347022e3fd364468da6e48ed
Guranda Jikia <jikiaguranda@gmail.com>
2025-02-22 11:27:47 +0400

    feat:impelemt flip_card functionality

--- main.py
+++ main.py
@@ -2,27 +2,36 @@ from tkinter import *
 import pandas 
 import random
 
+BACKGROUND_COLOR = "#B1DDC6"
+
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient ='records')
-
+current_card = {}
 
 
 def next_card():
-    current_word = random.choice(to_learn)
-    canvas.itemconfig(card_title, text = "French")
-    canvas.itemconfig(card_word, text = current_word["French"])
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text = "French", fill = "black")
+    canvas.itemconfig(card_word, text = current_card["French"], fill = "black")
+    canvas.itemconfig(card_back_img,image = card_front_img)
+    flip_timer = window.after(3000, func = flip_card)
 
+def flip_card():
+    canvas.itemconfig(card_title, text = "English", fill = "white")
+    canvas.itemconfig(card_word, text = current_card["English"], fill = "white")
+    canvas.itemconfig(card_front_img,image = card_back_img)
 
-
-BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title=("Flashy")
 window.config(padx = 50, pady = 50,bg = BACKGROUND_COLOR)
-
+flip_timer = window.after(3000, func = flip_card)
 
 canvas = Canvas(height=526, width = 800)
 card_front_img = PhotoImage(file = "images/card_front.png")
+card_back_img = PhotoImage(file = "images/card_back.png")
 canvas.create_image(400,263,image = card_front_img)
 canvas.config(bg = BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row = 0, columnspan=2)
@@ -37,6 +46,7 @@ check_image = PhotoImage(file="images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=1)
 next_card()
+# flip_card()
 
 
 
